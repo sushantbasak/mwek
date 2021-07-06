6795834d6b1bc0f8a6c66c5f0dc532cc4d507257
sushantbasak <sushantbasak73@gmail.com>
2021-07-06 11:24:45 +0530

    Add Collection-Preview Component in Shop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
-
 import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component.jsx';
-
-const HatsPage = () => <h1>Hello From Hats Page</h1>;
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </>
   );
