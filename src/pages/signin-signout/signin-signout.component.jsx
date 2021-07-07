7fb237633ec6eb3c43843b8581e70085c33b6c76
sushantbasak <sushantbasak73@gmail.com>
2021-07-07 11:32:31 +0530

    Add Custom Button & Form Input Component

---
+++ src/pages/signin-signout/signin-signout.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './signin-signout.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignOut = () => (
+  <div className="signin-and-signout">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignOut;
