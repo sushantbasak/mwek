93ae574ce97d14e0f1a58c4a0f77ad984bf8f286
sushantbasak <sushantbasak73@gmail.com>
2021-07-15 12:17:31 +0530

    Add Reselect, Replace with Memoized Redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,12 +5,14 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -18,4 +20,8 @@ const mapDispatchProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchProps)(CartIcon);
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchProps)(CartIcon);
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './cart-item.styles.scss';
 
-const CartItem = ({ imageUrl, name, price, quantity }) => (
+const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
   <div className="cart-item">
-    <img stc={imageUrl} alt="item" />
+    <img src={imageUrl} alt="item" />
     <div className="item-details">
       <span className="name">{name}</span>
       <span className="price">
